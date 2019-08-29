4e31d2b4cf1fcd0f74e39365a0f016b5e4c9069d
Omar <omar.musmanno@linksolution.com.ar>
2019-08-29 17:59:16 -0300

    Agregado de Maps

---
+++ MenuIndio/MenuIndio/Views/MapsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MenuIndio.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapsPage : ContentPage
+    {
+        public MapsPage()
+        {
+            InitializeComponent();
+        }
+        private async void ButtonOpenCoords_Clicked(object sender, EventArgs e) {
+            await Map.OpenAsync(55, 213, new MapLaunchOptions()
+           /* {
+                NamedSize = EntryName.Text,
+                Navigationmode = NavigationMode.None;
+            }*/);
+           
+        }
+        public async Task NavigateToBuilding25()
+        {
+            var location = new Location(47.645160, -122.1306032);
+            var options = new MapLaunchOptions { Name = "Microsoft Building 25" };
+
+            await Map.OpenAsync(location, options);
+        }
+    }
+}
